19cdc045fa60c665cf4c8b047b8ae1c9fea7b123
Rogeriom49 <37877711+Rogeriom49@users.noreply.github.com>
2024-10-28 14:31:57 -0300

    Update globals.py

--- techChallange4/globals.py
+++ techChallange4/globals.py
@@ -3,21 +3,26 @@ import os
 from dotenv import load_dotenv, dotenv_values
 from prophet import Prophet
 from prophet.serialize import model_to_json, model_from_json
-
-load_dotenv()
+import requests
 
 # Cria o dataframe que será usado em todo o projeto
 global df
-df = pd.read_csv(os.getenv("DATAFRAME"), sep=",", parse_dates=['data'])
+df = pd.read_csv('https://raw.githubusercontent.com/Rogeriom49/4DTAT/main/techChallange4/datasets/dados_mes_ano.csv', sep=",", parse_dates=['data'])
 
 # Cria dataframe com a data como index
 global df_index
-df_index = pd.read_csv(os.getenv("DATAFRAME_DATA_INDEX"), parse_dates=['data'], index_col='data')
+df_index = pd.read_csv('https://raw.githubusercontent.com/Rogeriom49/4DTAT/refs/heads/main/techChallange4/datasets/dados_puros.csv', parse_dates=['data'], index_col='data')
 
 # Cria dataframe para o prophet
 global df_prophet
-with open("modelo_prophet.json", "r") as json_file:
-    model = model_from_json(json_file.read())
+# with open("https://raw.githubusercontent.com/Rogeriom49/4DTAT/refs/heads/main/techChallange4/modelo_prophet.json", "r") as json_file:
+#     model = model_from_json(json_file.read())
+
+response = requests.get("https://raw.githubusercontent.com/Rogeriom49/4DTAT/refs/heads/main/techChallange4/modelo_prophet.json")
+model_json = response.text
+
+model = model_from_json(model_json)
+
 
 future = model.make_future_dataframe(periods=365)
 forecast = model.predict(future)
