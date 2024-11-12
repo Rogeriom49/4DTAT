20018b2f0a3de8f11b2f9f0a909f8c2ff0558774
Rogeriom49 <37877711+Rogeriom49@users.noreply.github.com>
2024-11-12 18:59:59 -0300

    Inserção da introdução, criação da pasta notebooks para centralização dos arquivos de analise

--- techChallange4/paginas/introducao.py
+++ techChallange4/paginas/introducao.py
@@ -1,5 +1,17 @@
 import streamlit as st
 
-st.write("""
-# Hello, Tech Challenge 4!
-""")
+st.header("""Tech Challenge 4!""")
+st.write(""" O petróleo é uma das commodities mais importantes do mundo, sendo essencial para diversas indústrias e influenciando significativamente a economia global. Com a complexidade crescente do mercado de petróleo e a volatilidade dos preços, a análise de dados dessa commodity tem se tornado cada vez mais relevante para identificar padrões, entender flutuações de preços e prever tendências futuras. """)
+
+st.write(""" Este projeto de análise de dados tem como objetivo explorar dados relacionados ao mercado de petróleo para extrair insights e desenvolver um modelo de machine learning que possa contribuir na previsão de preços e na tomada de decisões estratégicas. Através de técnicas de análise estatística e aprendizado de máquina, buscamos compreender variáveis chave que influenciam os preços, como oferta e demanda, eventos geopolíticos, entre outros fatores. """)
+
+st.write(""" As etapas principais do projeto incluem: """)
+
+st.write(""" 1. Coleta e Pré-Processamento dos Dados: Obtenção de dados históricos de preços de petróleo e de variáveis correlacionadas, além de seu tratamento para garantir a qualidade e consistência dos dados.""")
+
+st.write(""" 2. Exploração de Dados e Análise de Tendências: Utilização de técnicas de análise exploratória para identificar padrões históricos, sazonalidades e potenciais correlações entre as 
+variáveis.""")
+
+st.write(""" 3. Desenvolvimento de Modelo de Machine Learning: Aplicação de algoritmos de aprendizado de máquina para criar um modelo que seja capaz de prever preços futuros ou identificar padrões de comportamento do mercado com base nas variáveis analisadas.""")
+
+st.write(""" Ao final, este projeto fornecerá insights que poderão ser usados tanto para previsões do preço do petróleo quanto para orientar a tomada de decisões estratégicas no setor energético. Além disso, o modelo de machine learning desenvolvido poderá ser utilizado para simulações e análise de cenários futuros, contribuindo para uma melhor compreensão do mercado de petróleo.""")
