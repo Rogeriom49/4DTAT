a7cd0c00671cba7f50ed7b6679920c0fe6fd9ade
Rogeriom49 <37877711+Rogeriom49@users.noreply.github.com>
2024-11-18 20:56:36 -0300

    Ajuste url dashboard

--- techChallange4/paginas/dashboard.py
+++ techChallange4/paginas/dashboard.py
@@ -3,7 +3,7 @@ import streamlit.components.v1 as components
 
 st.header('**Dashboard**')
 
-iframe_url = "https://app.powerbi.com/reportEmbed?reportId=61457650-2823-4dce-a1b7-10faa41edbc7&autoAuth=true&ctid=6dc874ce-dd2b-4aa9-9f0d-adb9269a7580"
+iframe_url = "https://app.powerbi.com/view?r=eyJrIjoiYmZhZTk3ZjgtMDNkZi00NzU2LTlkYjEtZjUxZDcwODkyY2NlIiwidCI6IjZkYzg3NGNlLWRkMmItNGFhOS05ZjBkLWFkYjkyNjlhNzU4MCJ9"
 
 components.iframe(iframe_url, width=1400, height=650)
 
